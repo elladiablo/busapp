41926b64ca986891e57fc8d8a563e6b0c729923f
elladiablo <elladiablo@gmail.com>
2021-05-20 21:49:41 +0800

    Update

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import {useState} from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+import React, {useState} from 'react';
+import { ActivityIndicator, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 export default function App() {
   const[loading, setloading] =useState(true);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}> Bus Arrival Time</Text>
+      <Text style={styles.title}> Bus Arrival Time:</Text>
       <Text></Text>
       <Text style={styles.arrivalTime}>
-        {loading ? "Loading...": "Loaded"}</Text>
+        {loading ? <ActivityIndicator size ="large"/>:"Loaded"}</Text>
       <TouchableOpacity style={styles.button}>
         <Text style ={styles.buttonText}>Refresh!</Text>
       </TouchableOpacity>
@@ -26,5 +26,27 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  button: {
+    backgroundColor: 'green',
+    padding: 20,
+    borderRadius: 10,
+    marginTop:20
+  },
+
+  title: {
+    color: 'black',
+    fontWeight: 'bold',
+    marginBottom: 20,
+    fontSize:30
+  },
+
+  arrivalTime: {
+    color: 'black',
+    fontWeight: 'bold',
+    marginBottom: 20,
+    fontSize:30
+  }
+
 
 });
