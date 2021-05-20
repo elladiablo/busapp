22697fa6f194312820d8a0ff186a4d5af93ec4fc
elladiablo <elladiablo@gmail.com>
2021-05-20 20:56:46 +0800

    First commit

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {useState} from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+  const[loading, setloading] =useState(true);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}> Bus Arrival Time</Text>
+      <Text></Text>
+      <Text style={styles.arrivalTime}>
+        {loading ? "Loading...": "Loaded"}</Text>
+      <TouchableOpacity style={styles.button}>
+        <Text style ={styles.buttonText}>Refresh!</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -18,4 +26,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
 });
