0b6bb0864769d2763cd762b75849dc1ab6b98b08
elladiablo <elladiablo@gmail.com>
2021-05-29 22:11:18 +0800

    Final Project

--- App.js
+++ App.js
@@ -1,17 +1,49 @@
 
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import { ActivityIndicator, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 export default function App() {
   const[loading, setloading] =useState(true);
+  const[arrival,setArrival] =useState("");
+  const BUSSTOP_URL = "https://arrivelah2.busrouter.sg/?id=83139";
+
+  function loadBusStopData () {
+    setloading(true);
+
+    fetch (BUSSTOP_URL)
+    .then ((response) => {
+      //console.log(response.json)
+      return response.json();
+    })
+    .then ((responseData)=> {
+      //console.log("Original Data:");
+        const myBus = responseData.services.filter(
+        (item)=> item.no === "155"
+        
+      )[0];
+
+      setArrival(myBus.next.time)
+      setloading(false)
+    
+      });
+
+  }
+
+  useEffect(()=> {
+    const interval =setInterval(loadBusStopData,2000);
+
+    //Return the function to run when unmounting
+    return ()=> clearInterval(interval);
+
+  },[]);
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}> Bus Arrival Time:</Text>
       <Text></Text>
-      <Text style={styles.arrivalTime}>
-        {loading ? <ActivityIndicator size ="large"/>:"Loaded"}</Text>
+            <Text style={styles.arrivalTime}>
+        {loading ? <ActivityIndicator size ="large"/>:arrival}</Text>
       <TouchableOpacity style={styles.button}>
         <Text style ={styles.buttonText}>Refresh!</Text>
       </TouchableOpacity>
